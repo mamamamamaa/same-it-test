589f28a9cd9d69c74d0012775df210f514d32273
mamamamamaa <maks.denishchuk@gmail.com>
2023-03-11 13:12:33 +0200

    add departure thunk

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,11 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useAppDispatch } from "../redux/hooks";
+import { getDepartures } from "../redux/departuresSlice/operations";
 
 export const App: FC = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getDepartures({ city: "Одеса" }));
+  }, []);
   return <h1>Hello world</h1>;
 };
---
+++ src/redux/departuresSlice/operations.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const { VITE_API_KEY, VITE_BASE_URL } = import.meta.env;
+
+interface Properties {
+  city: string;
+  search?: string;
+}
+
+export interface ResponseDeparture {
+  Description: string;
+}
+
+export const getDepartures = createAsyncThunk<
+  ResponseDeparture[],
+  Properties,
+  { rejectValue: string }
+>("departures/getAll", async ({ city, search = "" }, thunkAPI) => {
+  const fetchParams = {
+    apiKey: VITE_API_KEY,
+    modelName: "Address",
+    calledMethod: "getWarehouses",
+    methodProperties: {
+      CityName: city,
+      Page: 1,
+      Limit: 100,
+      FindByString: search,
+    },
+  };
+
+  try {
+    const { data: res } = await axios.post(VITE_BASE_URL, fetchParams);
+    return res.data;
+  } catch (e) {
+    if (e instanceof Error) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+});
