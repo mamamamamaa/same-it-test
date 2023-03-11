ad0161b9d1e6f747b840a6c14560badfc8a72f17
mamamamamaa <maks.denishchuk@gmail.com>
2023-03-11 11:05:31 +0200

    add and configure mui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { App } from "./components/App";
+
+import theme from "./theme";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
