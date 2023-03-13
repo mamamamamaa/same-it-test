2ad4b948eba0f1fc774a95348887c0397c4f93d9
mamamamamaa <maks.denishchuk@gmail.com>
2023-03-13 10:27:20 +0200

    remove lazy loading

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,12 @@
-import { FC } from "react";
+import { FC, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { Layout } from "./Layout";
 
-import HomePage from "../pages/HomePage";
-import TrackingPage from "../pages/TrackingPage";
-import WarehousesPage from "../pages/WarehousesPage";
-//
+const HomePage = lazy(() => import("../pages/HomePage"));
+const TrackingPage = lazy(() => import("../pages/TrackingPage"));
+const WarehousesPage = lazy(() => import("../pages/WarehousesPage"));
+
 // const HomePage = lazyImport("../pages/HomePage");
 // const TrackingPage = lazyImport("../pages/TrackingPage");
 // const WarehousesPage = lazyImport("../pages/WarehousesPage");
